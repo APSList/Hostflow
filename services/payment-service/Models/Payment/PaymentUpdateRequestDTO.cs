707f6206138aca00bc9c0565482204d2d113ccee
anzeljc7 <tilen.anzeljc7@gmail.com>
2025-12-21 13:36:04 +0100

    Removed services after repo separation

--- services/payment-service/Models/Payment/PaymentUpdateRequestDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace payment_service.Models.Payment;
-
-public class PaymentUpdateRequestDTO
-{
-    public decimal Amount { get; set; }
-    public string Status { get; set; } = string.Empty;
-}
